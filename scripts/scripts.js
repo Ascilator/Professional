6cab327400671edd14ef14c734741298809d3503
Vlad <67093050+Ascilator@users.noreply.github.com>
2021-03-30 21:22:40 +0300

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -113,4 +113,50 @@ let burger = () => {
         body.classList.toggle('_lock');
     })
 }
+let dynamicAdaptive = () => {
+    let item = document.querySelector('.js_contuct_item');
+    let where_insert_item = document.querySelector('nav');
+    let first_pos_table = document.querySelector('.contuct_part');
+
+
+    let vieportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+    if (vieportWidth < 850) {
+        where_insert_item.append(item);
+    } else {
+        first_pos_table.append(item);
+    }
+
+    window.addEventListener('resize', function () {
+        vieportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+        if (vieportWidth < 850) {
+            where_insert_item.append(item);
+        } else {
+            first_pos_table.append(item);
+        }
+    })
+}
+
+AOS.init({
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 0, // offset (in px) from the original trigger point
+    delay: 200, // values from 0 to 3000, with step 50ms
+    duration: 800, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
+dynamicAdaptive();
 burger();
